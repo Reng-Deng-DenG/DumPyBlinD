40badba77b467767df6a4380cea60aeac6145f73
Project777 <60116535+Reng-Deng-DenG@users.noreply.github.com>
2020-08-31 11:35:26 +0200

    Create mysql.py

---
+++ data/mysql.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+
+mysql =  {
+	'escape': ['\'', '"', ''],
+	'comment' : ['-- -', '--+', '#', ' '],
+	'operator': ['AND'],
+	'functionV': '@@VERSION=@@VERSION',
+	'functionT': 'SLEEP({time})'
+
+}
